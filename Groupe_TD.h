34cc2c922369a9233e4c1df5328dfc215c550558
SarahHidalgo <96008121+SarahHidalgo@users.noreply.github.com>
2022-05-29 20:58:33 +0200

    Add files via upload

--- Groupe_TD.h
+++ Groupe_TD.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+
 #include "Etudiants.h"
 #include "FiseTSE.h"
 
@@ -11,18 +12,21 @@ using namespace std;
 class Groupe_TD : public FiseTSE
 {
 	string nom_TD_;
-	vector<Etudiant *> tabMembre_;
+	vector<Etudiant*> tabMembre_;
 
 public:
 	Groupe_TD(const string& nom = "");
 	~Groupe_TD();
 	void ajouterMembre(Etudiant* e);
 	const string& getNom() const { return nom_TD_; };
+	int getSize() const { return tabMembre_.size(); };
 	void afficher();
 	void ajoutEtudiants(FiseTSE Fise_info);
-	string getEtudiantImage(int i) { return tabMembre_[i]->getImage(); };
+	void sauver(ofstream& ofs) const;
+	void charger(ifstream& ifs);
+	string getEtudiantImage(int i) { return tabMembre_[i]->getImg(); };
 	string getEtudiantNomPrenom(int i) { return tabMembre_[i]->getNomPrenom(); };
-	int getSize() const { return tabMembre_.size(); };
+	//Etudiant getEtudiant(int i) { return *tabMembre_[i]; };
 	void afficherEcran(Groupe_TD);
 };
 
