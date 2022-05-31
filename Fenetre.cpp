072e8da27db195e7d24d4a4739fc1b13af0af16e
mariefroute <105056478+mariefroute@users.noreply.github.com>
2022-05-31 14:27:42 +0200

    Add files via upload

--- Fenetre.cpp
+++ Fenetre.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 Fenetre::Fenetre(string lettre_gr)
 {
-	nom_fenetre_ = "Groupe "+ lettre_gr;
+	nom_fenetre_ = "Groupe " + lettre_gr;
 }
 
 // affichage de la fenetre du groupe entrer par l'utilisateur
@@ -40,13 +40,12 @@ void Fenetre::afficherFenetre(Groupe_TD groupe, Portrait portrait)
 
 	int Nmax = groupe.getSize(); // nombre d'étudiants dans le groupe
 	int nb = 0; // on part de l'étudiant 0
+	int num_page = 1; // compteur de page
+	bool der_page = false; // booléen qui détermine si on a atteint la dernière page
+	Portrait portraitFleche;
+
 
 	app.clear(Color::White);
-	// On définit par défault le status d'absent aux élèves
-	string presence = "a.png";
-	Portrait portraitText;
-	portraitText.SetNom(presence);
-
 
 	for (int i = 0; i < 2; i++)
 	{
@@ -60,31 +59,26 @@ void Fenetre::afficherFenetre(Groupe_TD groupe, Portrait portrait)
 			portrait.afficherText(groupe.getEtudiantNom(nb), groupe.getEtudiantPrenom(nb), app, 150 + j * 270, 430 + i * 450);
 
 			nb++; // on passe à l'étudiant suivant en incrémentant l'entier n
-			portraitText.afficherImage(app, 150 + j * 270, 100 + i * 450, true);
 		}
 	}
 
+	
 
-	// On place les flèches de navigation
-	Portrait portraitFleche;
+	// On place les flèches de navigation que à droite car on est à la première page donc il n'y a rien à gauche
 	portraitFleche.SetNom("fleche_droite.png");
 	portraitFleche.afficherImage(app, 1850, 490, true);
-	//portraitFleche.SetNom("fleche_gauche.png");
-	//portraitFleche.afficherImage(app, 20, 490, true);
-
-
-
-
 
 	app.display();
 
-	int nb_page = 1;
 	int nb_restant;
 	nb_restant = Nmax - nb;
 	int current_nb = 0;
 
 	while (app.isOpen())
 	{
+		Portrait portraitText;
+		string presence = "p.png";
+
 		vector <int> etudiants_present;
 		Event event;
 
@@ -94,26 +88,45 @@ void Fenetre::afficherFenetre(Groupe_TD groupe, Portrait portrait)
 			// Appuis sur la croix
 			if (event.type == Event::Closed)
 				app.close();
-			// Appui sur le bouton gauche
-			if ((event.type == Event::MouseButtonPressed) && (event.mouseButton.button == Mouse::Left))
-			{
-				Vector2i localPosition = Mouse::getPosition(app);
 
-				// Navigation 
-				if (localPosition.x >= (1750)) // Clique sur la flèche de droite ( partie droite de la fenetre)
+			/// Appui sur le bouton gauche de la souris
+			if ((event.type == sf::Event::MouseButtonPressed) && (event.mouseButton.button == sf::Mouse::Left))
+			{
+				bool present = false;
+
+				Vector2i localPosition = Mouse::getPosition(app); // on récupère la position de la souris
+
+				for (int i = 0; i < 2; i++)
+				{
+					for (int j = 0; j < 6; j++)
+					{
+						// Si clique sur un étudiant
+						if ((localPosition.x >= (150 + j * 270) && localPosition.x <= (390 + j * 300)) && (localPosition.y >= (100 + i * 450) && localPosition.y <= (440 + i * 450)))
+						{
+							Portrait portraitText;
+							portraitText.SetNom(presence);
+							portraitText.afficherImage(app, 150 + j * 270, 100 + i * 450, true);
+						}
+						// Si clique sur la fleche
+					}
+				}
+
+
+				/// Clique sur la flèche de droite
+				if (!der_page && (localPosition.x >= (1750) && localPosition.y <= (540) && localPosition.y >= (490))) 
 				{
 					app.clear(Color::White);
+					current_nb = 0;
+					num_page++;
 
 					portraitFleche.SetNom("fleche_gauche.png");
 					portraitFleche.afficherImage(app, 20, 490, true);
 
-					//while (nb_restant >= 0 && current_nb<12)
-					
 					for (int i = 0; i < 2; i++)
 					{
 						for (int j = 0; j < 6; j++)
 						{
-							if (nb_restant >= 0 && current_nb < 12 && nb<25)
+							if (nb_restant >= 0 && current_nb < 12 && nb < groupe.getSize())
 							{
 								// Affichage photos des étudiants
 								portrait.SetNom(groupe.getEtudiantImage(nb));
@@ -121,14 +134,11 @@ void Fenetre::afficherFenetre(Groupe_TD groupe, Portrait portrait)
 
 								// Affichage des noms des étudiants
 								portrait.afficherText(groupe.getEtudiantNom(nb), groupe.getEtudiantPrenom(nb), app, 150 + j * 270, 430 + i * 450);
-								
-								portraitText.afficherImage(app, 150 + j * 270, 100 + i * 450, true);
-
-								current_nb++; // numéro de l'étudiant sur la page courante
+								current_nb++;
 								nb++; // on passe à l'étudiant suivant en incrémentant l'entier nb
 								nb_restant = Nmax - nb;
 							}
-								
+
 						}
 					}
 					// s'il reste des éleves on affiche la fleche de droite 
@@ -137,34 +147,72 @@ void Fenetre::afficherFenetre(Groupe_TD groupe, Portrait portrait)
 						portraitFleche.SetNom("fleche_droite.png");
 						portraitFleche.afficherImage(app, 1850, 490, true);
 					}
-					app.display();
-					
-				}
-				current_nb = 0;
-
-
-
-				bool present = false;
-				// Affichage de la présence des eleves
-				for (int i = 0; i < 2; i++)
-				{
-					for (int j = 0; j < 6; j++)
+					if (nb_restant == 0)
 					{
-						// Si clique sur un étudiant
-						if ((localPosition.x >= (150 + j * 270) && localPosition.x <= (390 + j * 300)) && (localPosition.y >= (100 + i * 450) && localPosition.y <= (440 + i * 450)))
+						der_page = true;
+					}
+					app.display();
+
+				}
+
+
+				/// Clique sur la flèche de gauche 
+				if (num_page!=1 && (localPosition.x <= (200) && localPosition.y <= (540) && localPosition.y >= (490))) 
+				{
+					app.clear(Color::White); // on nettoie la fenêtre
+
+
+					current_nb = 0; // réinitialisation du compteur local
+					der_page = false; // si on était à la dernière page, on y est plus, sinon ça ne change rien
+					
+									  
+					// cette boucle permet de déterminer à quel étudiant il faut démarrer l'affichage
+					if (num_page==2) // si on est à la deuxième page
+					{
+						nb = 0; //on remet nb à 0 pour commencer l'affichage à l'étudiant 0, ce qui correspond bien à la première page
+					}
+					else if(num_page==3) // si on est à la troisème page
+					{
+						nb = 12; //on remet nb à 12 pour commencer l'affichage à l'étudiant 12, ce qui correspond bien à la deuxième page
+					}
+
+					
+					// On affiche par défaut les flèches de droite car si on va à gauche c'est qu'il y avait une page à droite
+					portraitFleche.SetNom("fleche_droite.png");
+					portraitFleche.afficherImage(app, 1850, 490, true);
+
+					num_page--; // on décrémente le numéro de la page
+
+					// s'il reste une page à gauche, on remet une flèche 
+					if (nb!=0)
+					{
+						portraitFleche.SetNom("fleche_gauche.png");
+						portraitFleche.afficherImage(app, 20, 490, true);
+					}
+
+					while (nb_restant >= 0 && current_nb < 12)
+					{
+						for (int i = 0; i < 2; i++)
 						{
-							if (present)
+							for (int j = 0; j < 6; j++)
 							{
-								//portraitText.SetNom("p.png");
-								//portraitText.
+								// Affichage photos des étudiants
+								portrait.SetNom(groupe.getEtudiantImage(nb));
+								portrait.afficherImage(app, 150 + j * 270, 100 + i * 450, false);
+
+								// Affichage des noms des étudiants
+								portrait.afficherText(groupe.getEtudiantNom(nb), groupe.getEtudiantPrenom(nb), app, 150 + j * 270, 430 + i * 450);
+								current_nb++;
+								nb++; // on passe à l'étudiant suivant en incrémentant l'entier nb
+								nb_restant = Nmax - nb;
 							}
-							//portraitText.SetNom(presence);
-							//portraitText.afficherImage(app, 150 + j * 270, 100 + i * 450, true);
-							//app.display();
 						}
 					}
+					app.display();
+
 				}
 			}
+			//portrait.afficherPresence(app, 150 + j * 270, 100 + i * 450, presence);
 		}
 	}
 }
@@ -180,10 +228,8 @@ for (int i = 0; i < 5; i++)
 			// Affichage photos des étudiants
 			portrait.SetNom(groupe.getEtudiantImage(nb));
 			portrait.afficherImage(app, 20 + j * 300, 20 + i * 450, false);
-
 			// Affichage des noms des étudiants
 			portrait.afficherText(groupe.getEtudiantNom(nb), groupe.getEtudiantPrenom(nb), app, 20 + j * 300, 350 + i * 450);
-
 			// on passe à l'étudiant suivant en incrémentant l'entier n
 			nb++;
 		}
